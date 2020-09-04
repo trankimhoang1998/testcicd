ccd16cdb792c1a21afaad65e21e621eae701fa45
trankimhoang1998 <43273983+trankimhoang1998@users.noreply.github.com>
2020-09-04 23:28:47 +0700

    test ci cd v9

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    CICD CICD
+                    CICD CICD CICD
                 </div>
 
                 <div class="links">
